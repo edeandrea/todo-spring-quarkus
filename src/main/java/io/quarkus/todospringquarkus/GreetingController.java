cfb9e58c3ddf7c801e15a6bb543858090883e9bd
Eric Murphy <ermurphy@redhat.com>
2020-11-07 15:54:55 -0500

    Add metrics

--- src/main/java/io/quarkus/todospringquarkus/GreetingController.java
+++ src/main/java/io/quarkus/todospringquarkus/GreetingController.java
@@ -1,5 +1,6 @@
 package io.quarkus.todospringquarkus;
 
+
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
+
 
     @Secured("user")
     @GetMapping
