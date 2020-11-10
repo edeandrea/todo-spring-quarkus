03b8f38727e6c5d70c773dd213b5dd129f294696
Eric Murphy <ermurphy@redhat.com>
2020-11-10 00:17:34 -0500

    Fix formatting

--- src/main/java/io/quarkus/todospringquarkus/TodoController.java
+++ src/main/java/io/quarkus/todospringquarkus/TodoController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/io/quarkus/todospringquarkus/TodoEntity.java
+++ src/main/java/io/quarkus/todospringquarkus/TodoEntity.java
@@ -9,32 +9,32 @@ import javax.persistence.Entity;
 @Table(name = "todo")
 public class TodoEntity {  
       
-  @Id @GeneratedValue  
-  private Long id;  
-  private String title;  
-  private boolean completed = false;
+    @Id @GeneratedValue  
+    private Long id;  
+    private String title;  
+    private boolean completed = false;
 
-  public Long getId() {
-      return this.id;
-  }
+    public Long getId() {
+        return this.id;
+    }
 
-  public void setId(Long id) {
-      this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public String getTitle() {
-      return this.title;
-  }
+    public String getTitle() {
+        return this.title;
+    }
 
-  public void setTitle(String title) {
-      this.title = title;
-  }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-  public boolean getCompleted() { 
-      return this.completed;
-  }
+    public boolean getCompleted() { 
+        return this.completed;
+    }
 
-  public void setCompleted(boolean completed) {
-      this.completed = completed;
-  }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
