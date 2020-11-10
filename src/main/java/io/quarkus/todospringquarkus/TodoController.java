30e7913d08f6279fbea709de2a03fe2406829737
Eric Murphy <ermurphy@redhat.com>
2020-11-10 10:18:59 -0500

    Working API

--- src/main/java/io/quarkus/todospringquarkus/TodoController.java
+++ src/main/java/io/quarkus/todospringquarkus/TodoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,11 +30,17 @@ public class TodoController {
     public TodoEntity findById(@PathVariable("id") Long id) {
         return todoRepository.findById(id).get();
     }
+
+    @PutMapping
+    @Transactional
+    public void update(@RequestBody TodoEntity resource) {
+        todoRepository.save(resource);
+    }
  
     @PostMapping
     @Transactional
-    public Long create(@RequestBody TodoEntity resource) {
-        return todoRepository.save(resource).getId();
+    public TodoEntity create(@RequestBody TodoEntity resource) {
+        return todoRepository.save(resource);
     }
  
     @DeleteMapping(value = "/{id}")
