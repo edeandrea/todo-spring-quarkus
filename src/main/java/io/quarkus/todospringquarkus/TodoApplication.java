1091f68df7441ef46cef01e8e5470f1bad154d5a
Eric Murphy <ermurphy@redhat.com>
2020-11-12 16:12:12 -0500

    Demo updates

--- src/main/java/io/quarkus/todospringquarkus/TodoApplication.java
+++ src/main/java/io/quarkus/todospringquarkus/TodoApplication.java
@@ -3,7 +3,6 @@ package io.quarkus.todospringquarkus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// THIS CLASS IS NOT NEEDED FOR QUARKUS. ONLY HERE FOR SPRING BOOT SUPPORT.
 @SpringBootApplication
 public class TodoApplication {
 
