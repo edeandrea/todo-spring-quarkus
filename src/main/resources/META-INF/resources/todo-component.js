58857ab644319583a6407ec278fa68779dd38ba1
Eric Murphy <ermurphy@redhat.com>
2020-11-22 22:25:59 -0500

    Move HTML + CSS to separate file

--- src/main/resources/META-INF/resources/todo-component.js
+++ src/main/resources/META-INF/resources/todo-component.js
@@ -1,97 +1,23 @@
-"use strict";
-
 class TodoComponent extends HTMLElement {
 
     constructor() {
         super();
-        this.shadow = this.attachShadow({mode: 'open'});
+        this.attachShadow({mode: 'open'});
 
-        let tmpl = document.createElement('template');
-        tmpl.innerHTML = `
-        <style>
-            input[type="text"] {
-                background: transparent;
-                color: white;
-            }
-
-            label > input[type="checkbox"] {
-                display: none;
-            }
-                
-            label > input[type="checkbox"] + *::before {
-                content: "";
-                display: inline-block;
-                vertical-align: bottom;
-                width: 2rem;
-                height: 2rem;
-                border-radius: 10%;
-                border-style: solid;
-                border-width: 0.1rem;
-                border-color: gray;
-                border-radius: 2rem;
-                margin-right: 0.5rem;
-            }
-                
-            label > input[type="checkbox"]:checked + *::before {
-                content: "✓";
-                line-height: 2rem;
-                font-weight: bold;
-                color: white;
-                text-align: center;
-                background: green;
-                border-color: green;
-            }
-
-            label > input[type="checkbox"]:checked + * > input[type="text"] {
-                color: silver;
-                text-decoration: line-through;
-            }
-                
-            label > input[type="checkbox"] + * > button {
-                visibility: hidden;
-            }
-                
-            label > input[type="checkbox"]:checked + * > button {
-                visibility: visible;
-                background: none;
-                border: none;
-                font-size: 1.3rem
-            }
-                
-            label > input[type="checkbox"] + * > input[type="text"] {
-                line-height: 1.5rem;
-                font-size: 1.5rem;
-                border: 0;
-                margin-bottom: 2px;
-            }
-
-            :host(:first-of-type) label > input[type="checkbox"] + * > input[type="text"] {
-                border: 1px solid gray;
-                border-radius: 6px;
-                margin-left: 3.1rem;
-            }
-            
-            :host(:first-of-type) label > input[type="checkbox"] + *::before {
-                display: none;
-            }
-        </style>
-        <form>
-            <label>
-                <input type="hidden" name="id">
-                <input type="checkbox" name="completed">
-                <span>
-                    <input type="text" name="title" required size="40" maxlength="80">
-                    <button type="button" name="delete">&#x274C;</button>
-                </span>
-            </label>
-        </form>`;
-
-        this.shadow.appendChild(tmpl.content.cloneNode(true));
+        fetch('./todo-component.html')
+        .then(stream => stream.text())
+        .then(html => {
+            let tmpl = document.createElement('template');
+            tmpl.innerHTML = html;
+            console.log(html);
+            this.shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this.attachEvents();
+        });
     }
 
-    connectedCallback() {
+    attachEvents() {
         const todo = this;
-        const form = this.shadow.querySelector('form');
+        const form = this.shadowRoot.querySelector('form');
         const idInput = form.elements['id'];
         const titleInput = form.elements['title'];
         const completedInput = form.elements['completed'];
